b543c90e9cb4799325cbb5b721b09358107870ec
IMRANDIL <aliimranadil2@gmail.com>
2022-07-28 11:28:56 +0530

    yh

--- dsa.js
+++ dsa.js
@@ -323,4 +323,18 @@ const targetSum = (arr, targetSum) => {
   }
 };
 
-console.log(targetSum(targetSumArray, 9));
+// console.log(targetSum(targetSumArray, 9));
+//Math algorithms....
+
+//first problem....Fibonacci sequence..
+
+// Given a number 'n', find the first n elements of the fibonacci sequence..
+
+// In mathematics, the fibonacci sequence is a sequence in which each number is the sum of the two preceding ones...
+
+//The first two numbers in the sequence are 0 and 1...
+
+//eg...fibonacci[2]=[0,1]
+// fibonacci[3] = [0,1,1]
+
+// Fibonacci[7] = [0,1,1,2,3,5,8]
