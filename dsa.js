62128535293886f2e2dff1ec4e7af32be5665f7f
IMRANDIL <aliimranadil2@gmail.com>
2022-07-29 13:39:56 +0530

    yh

--- dsa.js
+++ dsa.js
@@ -350,3 +350,7 @@ function fibonacci(n) {
 console.log(fibonacci(2)); //[0,1]
 console.log(fibonacci(3)); //[0,1,1]
 console.log(fibonacci(7)); //[0,1,1,2,3,5,8]
+
+//service workers are a proxy between browser and network,service worker can redirect requests to a cache,enabling offline access...
+
+//web workers are general purpose scripts that enable us to offload processor intensive work from the main thread..
